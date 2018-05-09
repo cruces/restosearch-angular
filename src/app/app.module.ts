458288601aef666d80c53ae0eb3dc4b15f312adb
Dumevi Cruces <dcs.dume@gmail.com>
2018-05-09 02:25:50 +0100

    Added 404 page. Added restaurant location. Fixed routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 
@@ -8,24 +7,26 @@ import {RestaurantComponent} from './restaurant/restaurant.component';
 
 import {HttpModule} from '@angular/http';
 import {RestaurantService} from './core/services/restaurant.service';
-import {CategoryComponent} from './category/category.component';
 import {CategoryService} from './core/services/category.service';
 import {FooterComponent, HeaderComponent, SharedModule} from './shared';
 import {RestaurantsComponent} from './restaurants/restaurants.component';
 import {HomeComponent} from './home/home.component';
 
-import { InfiniteScrollModule } from 'angular2-infinite-scroll';
+import {InfiniteScrollModule} from 'angular2-infinite-scroll';
+import {CategoryComponent} from './category/category.component';
+import {NotfoundComponent} from './notfound/notfound.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
         RestaurantComponent,
-        CategoryComponent,
         FooterComponent,
         HeaderComponent,
         RestaurantsComponent,
-        HomeComponent
+        HomeComponent,
+        CategoryComponent,
+        NotfoundComponent
     ],
     imports: [
         BrowserModule,
@@ -33,7 +34,6 @@ import { InfiniteScrollModule } from 'angular2-infinite-scroll';
         HttpModule,
         SharedModule,
         InfiniteScrollModule
-
     ],
     bootstrap: [AppComponent],
     providers: [RestaurantService, CategoryService]
